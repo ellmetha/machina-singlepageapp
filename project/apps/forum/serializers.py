aae01cdcbd239397dad46b2d5fac91eb4219479f
Morgan Aubert <morgan.aubert@zoho.com>
2016-03-23 23:07:27 -0400

    Add siblings to forum serializer

--- project/apps/forum/serializers.py
+++ project/apps/forum/serializers.py
@@ -10,6 +10,8 @@ Forum = get_model('forum', 'Forum')
 
 class ForumSerializer(serializers.ModelSerializer):
     description = serializers.SerializerMethodField()
+    previous_sibling = serializers.SerializerMethodField()
+    next_sibling = serializers.SerializerMethodField()
 
     class Meta:
         model = Forum
@@ -17,7 +19,16 @@ class ForumSerializer(serializers.ModelSerializer):
             'id', 'name', 'slug', 'type', 'description', 'image', 'link', 'link_redirects',
             'posts_count', 'topics_count', 'link_redirects_count', 'last_post_on',
             'display_sub_forum_list', 'lft', 'rght', 'tree_id', 'level', 'parent',
+            'previous_sibling', 'next_sibling',
         ]
 
     def get_description(self, obj):
         return obj.description.rendered
+
+    def get_previous_sibling(self, obj):
+        sibling = obj.get_previous_sibling()
+        return sibling.pk if sibling else None
+
+    def get_next_sibling(self, obj):
+        sibling = obj.get_next_sibling()
+        return sibling.pk if sibling else None
